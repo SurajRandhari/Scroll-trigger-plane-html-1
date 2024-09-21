72826e2d3cdd783c9176046d78e2c1cee4066f96
Suraj Randhari <surajrandhari@gmail.com>
2024-09-21 14:05:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const lenis = new Lenis()
+
+function raf(time) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+requestAnimationFrame(raf)
+
+
+
+document.querySelectorAll('.card').forEach(card => {
+  gsap.to(card, {
+    scale:0.7,
+    opacity:0,
+    scrollTrigger: {
+      trigger: card,
+      start: "top 15%",
+      end: "bottom 15%",
+      markers: true,
+      scrub:true
+    }
+  })
+})
